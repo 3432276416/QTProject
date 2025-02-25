b11c6fd98573b57c1ef32bfe85d69a233a802b6f
Haru <3432276416@qq.com>
2025-02-25 23:30:06 +0800

    2.25

--- ListView/mainwindow.cpp
+++ ListView/mainwindow.cpp
@@ -13,6 +13,36 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+void MainWindow::openDataFromFile(QString filename)
+{
+    QStringList stringList;
+    QFile file(filename);
+    if(!file.open(QIODevice::ReadOnly||QIODevice::Text))
+    {
+        QTextStream textStream(&file);
+        while(!textSteam.atEnd())
+        {
+            QString line =textSteam.ReadLine();
+            stringList.append(line);
+        }
+    }
+}
+
+void MainWindow::initData(QStringList list)
+{
+    QString header=list.at(0);
+    QStringList headerList=header.split(QRegularExpression("\\s+"),Qt::SkipEmptyParts);
+     for(int i=0;i<list.size();i++)
+     {
+         QStringList stringList = list.at(i).split(",");
+         QString name = stringList.at(0);
+         QString age = stringList.at(1);
+         QString sex = stringList.at(2);
+         QString address = stringList.at(3);
+         QString phone = stringList.at(4);
+        QString email = stringList.at(5);
+     }
+}
 
 void MainWindow::on_actionacionOpen_triggered()
 {
